51f685a677af58f280264ca0da0e5605fb78877e
Matteo Figus <matteofigus@gmail.com>
2014-04-15 16:01:12 +0100

    Using unique Ids on permlinks in order to prevent strange behaviours

--- test/gherkin_report_test.js
+++ test/gherkin_report_test.js
@@ -6,12 +6,12 @@ var path = require('path');
 var inputDir = path.join(__dirname, 'fixtures');
 var outputDir = path.join(__dirname, 'output');
 
-exports.after_running_task = { 
+exports.after_running_task = {
   should_input_files_be_in_place: function(test) {
     test.expect(3);
     test.ok(grunt.file.exists(path.join(inputDir, 'AnotherFeature.feature')));
-    test.ok(grunt.file.exists(path.join(inputDir, 'MyFeature.feature')));
-    test.ok(grunt.file.exists(path.join(inputDir, 'SubCategory/Feature.feature')));
+    test.ok(grunt.file.exists(path.join(inputDir, 'My Feature.feature')));
+    test.ok(grunt.file.exists(path.join(inputDir, 'Sub Category/Feature.feature')));
     test.done();
   },
   should_the_output_files_be_produced: function(test) {
